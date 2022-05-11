b59d0e86f675524a703108574054116282e88d5e
andrea1632 <andrea.bellucci10@libero.it>
2022-05-11 14:52:19 +0200

    new class product

---
+++ classes/product.php
@@ -0,0 +1,41 @@
+<?php
+
+class Product
+{
+    protected $name;
+    protected $description;
+    protected $price;
+
+    public function __construct($name, $description, $price)
+    {
+        $this -> name = $name;
+        $this -> description = $description;
+        $this -> price = $price;
+
+        //setter
+
+        public function setName($_name){
+            $this->name = $_name;
+        }
+        public function setDescription($_description){
+            $this->description = $_description;
+        }
+        public function setPrice($_price){
+            $this->price = $_price;
+        }
+        
+        //getter
+
+        public function getName(){
+            return $this->name;
+        }
+        public function getDescription(){
+            return $this->description;
+        }
+        public function getPrice(){
+            return $this->price;
+        }
+    }
+}
+
+?>
